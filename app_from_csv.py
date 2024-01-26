31ed4ce7029797dd17e4d75f65c17cfbfba9fba9
js4000all <js4000all@gmail.com>
2024-01-26 11:20:37 +0000

    郵便番号一覧対応

--- app_from_csv.py
+++ app_from_csv.py
@@ -3,23 +3,23 @@ import dataclasses as dc
 import typing as ty
 
 @dc.dataclass
-class City:
+class ZipCode:
     code: str
-    name: str
     prefecture: str
-    prefecture_code: str
+    city: str
+    street: str
 
-def read_cities_from_csv(file_path: str) -> ty.Iterator[City]:
+def read_zipcode_from_csv(file_path: str) -> ty.Iterator[ZipCode]:
     with open(file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            city = City(code=row[0], name=row[1], prefecture=row[2], prefecture_code=row[3])
+            city = ZipCode(code=row[2], prefecture=row[6], city=row[7], street=row[8])
             yield city
 
 def main() -> None:
     # csv from https://postaladdress.jp/municipality/download
-    cities = read_cities_from_csv('data/2023-12-29.143241.5587175873201664418617.csv')
-    print(len(list(cities)))
+    codes = read_zipcode_from_csv('data/utf_ken_all.csv')
+    print(len(list(codes)))
 
 if __name__=='__main__':
     main()
